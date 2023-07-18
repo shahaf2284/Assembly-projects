40ae790803f1ca47d9959c509adfd162113c50f6
shahaf2284 <122786017+shahaf2284@users.noreply.github.com>
2023-07-18 22:10:15 +0300

    Add files via upload

---
+++ Lab5-pre/bsp.h
@@ -0,0 +1,24 @@
+#ifndef _bsp_H_
+#define _bsp_H_
+
+//#include <msp430xG46x.h>      //MSP430
+#include <msp430FG4619.h>      //MSP430
+//#include  <msp430G2553.h>
+
+#define   LcdsArrPort       &P1OUT
+#define   LcdsArrPortDir    &P1DIR
+#define   LcdsArrPortSel    &P1SEL
+
+#define   PBsArrPort        &P2OUT
+#define   PBsArrPortDir     &P2DIR
+#define   PBsArrPortSEL     &P2SEL
+
+
+;#define   PBsArrPort        &P2IN
+#define   PBsArrIntPend     &P2IFG
+#define   PBsArrIntEN       &P2IE
+#define   PBsArrIntEdeSEL   &P2IES
+#define   PBsArrPortSEL     &P2SEL
+#define   PBsArrPortDir     &P2DIR
+
+#endif
